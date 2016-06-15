2f4fee7c42de334626c0d6e6b0c30efeb6ca7997
nas4152 <nas4152@gmail.com>
2016-06-15 19:02:20 -0400

    loading data sets pt i

--- run_analysis.R
+++ run_analysis.R
@@ -7,3 +7,19 @@ if (!dir.exists("UCI HAR Dataset")) {
 }
 ## checked with and without data in wd
 
+## features.txt lists col names for observation variables
+## train or test/subject_test.txt col to add with individual ids
+## train or test/y_train or test.txt col to add with activity (see activity_
+##labels.txt for meaning)
+## inertial data: 128 variables b/c 128 readings per video
+
+activitylabels <- read.table("UCI HAR Dataset/activity_labels.txt")
+## first col is number, col2 is description
+variablelabels <- read.table("UCI HAR Dataset/features.txt")
+## col1 is index
+
+testobs <- read.table("UCI HAR Dataset/test/x_test.txt")
+testactivity <- read.table("UCI HAR Dataset/test/y_test.txt")
+
+id <- read.table("UCI HAR Dataset/test/subject_test.txt")
+## numbers to represent individuals
